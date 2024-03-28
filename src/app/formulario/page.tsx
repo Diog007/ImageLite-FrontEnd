4e06c1cca91639027b1610dd7377057b7ec99c5c
Diog007 <diogo.mz123@gmail.com>
2024-03-28 16:20:09 -0300

    Component creation

---
+++ src/app/formulario/page.tsx
@@ -0,0 +1,5 @@
+export default function FormularioPage() {
+    return (
+        <h1>Formulário</h1>
+    )
+}
---
+++ src/app/galeria/page.tsx
@@ -0,0 +1,5 @@
+export default function GaleriaPage(){
+    return(
+        <h1>Galeria</h1>
+    )
+}
