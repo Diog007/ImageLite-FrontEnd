5ad63e6320136484e642a0b366df8d4e4da79b27
Diog007 <diogo.mz123@gmail.com>
2024-04-25 15:03:53 -0300

    Image download button implemented

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -8,13 +8,18 @@ interface ImageCardProps {
 export const ImageCard: React.FC<ImageCardProps> =
  ({nome, dataUpload, src, tamanho }: ImageCardProps) => {
 
+    function download(){
+        window.open(src, '-blank')
+    }
+
     return (
-        <div className="card relative bg-white rounded-md shadow-md transition-tranform ease-in duration-300 transform hover:shadow-lg hover:-translate-y-4">
+        <div className="card relative bg-white rounded-md shadow-md transition-tranform ease-in duration-300 transform hover:shadow-lg hover:-translate-y-2 ">
             <img src={src} className="h-56 w-full object-cover rounded-t-md" alt="" />
-            <div className="card-body p-4">
+            <div className="card-body p-4 ">
                 <h5 className="text-xl font-semibold mb-2 text-gray-600">{nome}</h5>
                 <p className="text-gray-600">{tamanho}</p>
                 <p className="text-gray-600">{dataUpload}</p>
+                <button onClick={download} className='bg-red-500 text-white px-2 py-1 rounded-lg text-sm mx-auto block'>DOWNLOAD</button>
             </div>
         </div>
     )
