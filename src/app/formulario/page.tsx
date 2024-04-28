c3a8734aa3176f4efcb2e4dc798fdf97a79c5fb0
Diog007 <diogo.mz123@gmail.com>
2024-04-28 13:52:15 -0300

    Image upload with preview implemented

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { InputText, Template, Button } from "@/components"
+import { InputText, Template, Button, RenderIf } from "@/components"
 import Link from "next/link"
 import { useFormik } from "formik"
+import { useState } from "react";
 
 interface FormProps {
     name: string;
@@ -15,12 +16,24 @@ const formScheme: FormProps = { name: '', tags: '', file: '' }
 
 export default function FormularioPage(){
 
+    const [imagePreview, setImagePreview] = useState<string>();
+
+
     const formik = useFormik<FormProps>({
         initialValues: formScheme,
         onSubmit: (dados: FormProps) => {
             console.log(dados)
         }
     })
+
+    function onFileUpload(event: React.ChangeEvent<HTMLInputElement>){
+        if(event.target.files){
+            const file = event.target.files[0]
+            formik.setFieldValue("file", file)
+            const imageURL = URL.createObjectURL(file)
+            setImagePreview(imageURL)
+        }
+    }
 
     return (
         <>
@@ -46,23 +59,32 @@ export default function FormularioPage(){
                         <div className="mt-5 grid grid-cols-1">
                             <label className="block text-sm font-medium leading-6 text-gray-700">Image: *</label>
                             <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                                
                                 <div className="text-center ">
 
-                                    <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                                        <path fillRule="evenodd" 
-                                                d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5
-                                                6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0
-                                                .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12
-                                                0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3
+                                    <RenderIf condition={!imagePreview}>
+                                        <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                            <path fillRule="evenodd" 
+                                                    d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5
+                                                    6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0
+                                                    .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12
+                                                    0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3
                                                     16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" 
-                                            clipRule="evenodd" />
-                                    </svg>
+                                                    clipRule="evenodd" />
+                                        </svg>
+                                    </RenderIf>
 
                                     <div className="mt-4 flex text-sm leading-6 text-gray-600">
                                         <label className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600">
-                                            <span>Click to upload</span>
-                                            <input type="file" className="sr-only"></input>
+                                            <RenderIf condition={!imagePreview}>
+                                                <span>Click to upload</span>
+                                            </RenderIf>
+
+                                            <RenderIf condition={!!imagePreview}>
+                                                <img src={imagePreview} width={250} className="ro unded-md"/>
+
+                                            </RenderIf>
+
+                                            <input onChange={onFileUpload} type="file" className="sr-only"></input>
                                         </label>
                                     </div>
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { Template } from './Template'
+export { Template, RenderIf } from './Template'
 export { ImageCard } from './ImageCard'
 export { Button } from './button'
 export { InputText } from './input'
